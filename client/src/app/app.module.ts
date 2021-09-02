4abb0fd792dc93e9eedfa433a6dab08a5f1da9e3
odei mzabi <odeimzabi2000@gmail.com>
2021-09-01 17:07:44 -0700

    added navbar and updated components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { ProjectsComponent } from './components/projects/projects.component';
+import {MatSelectModule} from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core/option';
+import {FormsModule} from "@angular/forms";
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProjectsComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
